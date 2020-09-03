1e1471d343c1e7580ba22e97f3f9bd55446a6203
Genesis Villalvazo <gdv0001@auburn.edu>
2020-09-02 19:05:41 -0500

    Added MenuStrip control and items and sub-items for menuStrip.

--- WebBrowser.UI/Form1.cs
+++ WebBrowser.UI/Form1.cs
@@ -16,5 +16,10 @@ namespace WebBrowser.UI
         {
             InitializeComponent();
         }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
